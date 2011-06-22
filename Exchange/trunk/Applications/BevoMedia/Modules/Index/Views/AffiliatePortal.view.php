0955b29bd20f41fec1ae33ff4a7ece047566e0ec
bobbsoap <bobbsoap@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-06-22 21:59:10 +0000

    x: affportal page styling

---
+++ Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/AffiliatePortal.view.php
@@ -0,0 +1,121 @@
+<h2>BevoMedia Affiliate Portal</h2>
+
+<p>Bevo's Affiliate Portal is a state of the art internet advertising consolidation platform which consolidates all of your affiliate marketing efforts. The free application effectively reduces the hardest and most time consuming aspects of affiliate marketing. BevoMedia literally lets publishers handle any and all of their needs from one central interface.</p>
+
+<h2>Features include:</h2>
+
+<div class="feats">
+	<h2 class="networks">Affiliate networks comparison</h2>
+	<ul>
+		<li><span>Apply &amp; get accepted</span> into affiliate networks right from within the Bevo Interface.</li>
+		<li><span>Compare</span> the performance of your networks with Bevo's detailed analysis, including graphs and tables.</li>
+	</ul>
+	<div class="sepa"></div>
+	
+	<h2 class="offers">Affiliate offer management</h2>
+	<ul>
+		<li><span>Search, compare, and run</span> affiliate offers right from your account. Bevo's revolutionary <a href="http://offers.bevomedia.com/" target="_blank">Offer Hub</a> is integrated directly into the Affiliate Portal.</li>
+		<li><span>Get</span> your affiliate tracking links for offers right from the interface.</li>
+	</ul>
+	<div class="sepa"></div>
+	
+	<h2 class="ppc">PPC Management</h2>
+	<ul>
+		<li><span>Manage</span> your Google Adwords, Microsoft AdCenter (Bing/Yahoo), and more accounts.</li>
+		<li><span>Get</span> detailed stats and analytics using our API.</li>
+	</ul>
+	<div class="sepa"></div>
+	
+	<h2 class="keywords">Affiliate Marketing Keyword Tracker</h2>
+	<ul>
+		<li><span>Track</span> your keywords and see which ones outperformed others.</li>
+		<li><span>Optimize</span> your campaigns easily using valuable insights.</li>
+	</ul>
+	<div class="sepa"></div>
+	
+	<h2 class="analytics">Campaign Analytics Management</h2>
+	<ul>
+		<li><span>Track</span> your keywords and see which ones outperformed others.</li>
+		<li><span>Optimize</span> your campaigns easily using valuable insights.</li>
+	</ul>
+	<div class="sepa"></div>
+	
+	<h2 class="classroom">Learn Affiliate Marketing</h2>
+	<ul>
+		<li><span>Track</span> your keywords and see which ones outperformed others.</li>
+		<li><span>Optimize</span> your campaigns easily using valuable insights.</li>
+	</ul>
+</div><!--close feats-->
+
+<p class="aligncenter">For more information, visual walkthoughs, and to sign up for free, go to <a href="http://bevomedia.com/">beta.bevomedia.com</a> now!</p>
+<a class="btn gotoaffportal" href="http://bevomedia.com/">Go to Affiliate Portal</a>
+
+<h2>Apps</h2>
+
+<p>In the Bevo Affiliate Portal's App Store, affiliates have the opportunity to purchase some of the best affiliate marketing tools in the industry - but we also have a lot of 100% free apps for all affiliates to use without any cost. BevoMedia prides itself on being a premium platform for quality affiliate marketers and offering tools that will take profits to the next level.</p>
+
+<p>In the Affiliate Portal, you'll find these apps ready to use:</p>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_ppvspy.png" alt="PPV Spy" />
+	<div class="floatleft">
+		<h2>Bevo PPV Spy</h2>
+		<p>PPV Spy is the only research tool for Pay-Per-View ads in existence. By using it, you will get unique insights into what works for your competitors - and what doesn't. It's all at your fingertips and ready to be replicated.</p>
+	</div>
+	<div class="clear"></div>
+</div>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_geo.png" alt="Geotargeting" />
+	<div class="floatleft">
+		<h2>Geotargeting</h2>
+		<p>Geotarget your landing pages and offers. This feature allows you to show different pages based on where the viewer is coming from in the world. Target by any city, state, or country in the world!</p>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="clear"></div>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_dayparting.png" alt="Dayparting" />
+	<div class="floatleft">
+		<h2>Dayparting &amp; Daytargeting</h2>
+		<p>Target your landing pages and offers by both day and hour! This feature allows you to see the time period your campaign converts the best at, and allows you to optimize and target accordingly.</p>
+	</div>
+	<div class="clear"></div>
+</div>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_ppv.png" alt="Keyword List Builder" />
+	<div class="floatleft">
+		<h2>Bevo Keyword List Builder</h2>
+		<p>The Bevo Keyword List Builder is a premium keyword research tool designed to save time during the crucial research stages of building a campaign. This tool is a huge leg up on the competition whether you have a search, PPV or media buy campaign.</p>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="clear"></div>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_ppccampeditor.png" alt="PPC Campaign Editor" />
+	<div class="floatleft">
+		<h2>PPC Campaign Editor</h2>
+		<p>Bevo's PPC Management gives you the opportunity not only to examine your search marketing expenses, but also to edit and create all of your campaigns within a single interface. Check out your search campaign stats, create or edit a campaign and gain an in-depth view of exactly where your money is going.</p>
+	</div>
+	<div class="clear"></div>
+</div>
+
+<div class="box">
+	<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/pagedesc_perfconn.png" alt="Performance Connector" />
+	<div class="floatleft">
+		<h2>Performance Connector</h2>
+		<p>The Bevo Performance Connector is a free service connecting networks with publishers who want the best opportunity of promoting an offer. Bevo Performance Connector members receive periodic introductory emails connecting them with their best fit partnered networks.</p>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="clear"></div>
+
+<br /><br />
+<p class="aligncenter">...and many more!</p>
+<a class="btn gotoaffportal" href="http://bevomedia.com/">Go to Affiliate Portal</a>
+
+<br /><br />
+<p class="aligncenter">Give your affiliate marketing campaigns an immediate boost and <a href="http://bevomedia.com/">sign up for free</a> right now!</p>
