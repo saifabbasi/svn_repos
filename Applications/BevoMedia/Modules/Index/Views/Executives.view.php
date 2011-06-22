d2a7935f1306a61d632884dbdb328286a079657b
bobbsoap <bobbsoap@rcs.co>
2011-06-22 21:59:10 +0000

    x: affportal page styling

--- Applications/BevoMedia/Modules/Index/Views/Executives.view.php
+++ Applications/BevoMedia/Modules/Index/Views/Executives.view.php
@@ -26,7 +26,7 @@
 	</div>
 </blockquote>
 <div class="anno">
-	<p>Michael Chambrello entered the online marketing space in 2008 when he began building and flipping incentivized websites, leading him to gain experience in search and social marketing. With this experience, Chambrello went on to co-found a short lived local search marketing company and to become a partner in the Bevo Media Exchange. While at Syracuse University, Chambrello studied English and Textual Studies and played football for the Orange.</p>
+	<p>Michael Chambrello entered the online marketing space in 2006 when he began building and flipping incentivized websites, leading him to gain experience in search and social marketing. With this experience, Chambrello went on to co-found a short lived local search marketing company and to become a partner in the Bevo Media Exchange. While at Syracuse University, Chambrello studied English and Textual Studies and played football for the Orange.</p>
 	<p>Michael Chambrello is from Southington, CT and currently resides in San Diego, CA.</p>
 </div>
 
@@ -38,13 +38,13 @@
 	<div class="floatleft">	
 		<h3>Michael F. Vellucci</h3>
 		Chief Financial Officer and Co-Founder<br />
-		Maryland University<br />
+		University of Maryland<br />
 		<a href="mailto:mike@bevomedia.com">mike@bevomedia.com</a>
 		<img class="unicrest" src="/Themes/BevoMedia/Images/content_execs_crest_umd.gif" alt="University of Maryland" />
 	</div>
 </blockquote>
 <div class="anno">
-	<p>As the founder and president of Bevo Media, Ryan Bukevicz has been in the online marketing industry since 2001. Bukevicz has extensive experience in various types of media buying, search and social marketing emassing revenues exceeding $10MM.  Bukevicz is also known to be involved in several other startups in internet marketing and mobile application space. During his college years, Bukevicz was a 2 sport Division 1 athlete at Syracuse University playing football and running track for the Orange. At Syracuse, Bukevicz was selected the Entrepeneur of the Year voted by university faculty. An entrepeneur at heart, Bukevicz started his first company when he was just 14 years old. Hometown located in Ivyland, PA and now resides in San Diego, California. </p>
+	<p>Michael Vellucci has been in the online marketing industry since 2001, including co-founding Bevo Media with Ryan Bukevicz. The two have been business partners in various online ventures for over a decade. Along with his internet marketing background, Mr. Vellucci has an extensive background in finance, having worked in both investment banking and private equity. Mr. Vellucci graduated Summa Cum Laude from the University of Maryland with a double major in finance and accounting, with minor concentrations in economics and entrepreneurship.  At Maryland, Mr. Vellucci participated in the Hinman CEOs living-learning entrepreneurship program in which he won various awards and business plan competitions. Mr. Vellucci is originally from Richboro, PA, where he and Mr. Bukevicz built their first company.</p>
 </div>
 
 <div class="clear"></div>
