f859afd4f492a2c1de62b1a63ac4b0cf175b18db
bobbsoap <bobbsoap@rcs.co>
2011-06-22 20:58:49 +0000

    x: execs

--- Applications/BevoMedia/Modules/Index/Views/Executives.view.php
+++ Applications/BevoMedia/Modules/Index/Views/Executives.view.php
@@ -5,6 +5,7 @@
 		President and Co-Founder<br />
 		Syracuse University<br />
 		<a href="mailto:ryan@bevomedia.com">ryan@bevomedia.com</a>
+		<img class="unicrest" src="/Themes/BevoMedia/Images/content_execs_crest_syracuse.gif" alt="Syracuse University" />
 	</div>
 </blockquote>
 <div class="anno">
@@ -21,10 +22,12 @@
 		Managing Partner<br />
 		Syracuse University<br />
 		<a href="mailto:michael.chambrello@bevomedia.com ">michael.chambrello@bevomedia.com</a>
+		<img class="unicrest" src="/Themes/BevoMedia/Images/content_execs_crest_syracuse.gif" alt="Syracuse University" />
 	</div>
 </blockquote>
 <div class="anno">
-	<p>As the founder and president of Bevo Media, Ryan Bukevicz has been in the online marketing industry since 2001. Bukevicz has extensive experience in various types of media buying, search and social marketing emassing revenues exceeding $10MM.  Bukevicz is also known to be involved in several other startups in internet marketing and mobile application space. During his college years, Bukevicz was a 2 sport Division 1 athlete at Syracuse University playing football and running track for the Orange. At Syracuse, Bukevicz was selected the Entrepeneur of the Year voted by university faculty. An entrepeneur at heart, Bukevicz started his first company when he was just 14 years old. Hometown located in Ivyland, PA and now resides in San Diego, California.</p>
+	<p>Michael Chambrello entered the online marketing space in 2008 when he began building and flipping incentivized websites, leading him to gain experience in search and social marketing. With this experience, Chambrello went on to co-found a short lived local search marketing company and to become a partner in the Bevo Media Exchange. While at Syracuse University, Chambrello studied English and Textual Studies and played football for the Orange.</p>
+	<p>Michael Chambrello is from Southington, CT and currently resides in San Diego, CA.</p>
 </div>
 
 <div class="clear"></div>
@@ -37,6 +40,7 @@
 		Chief Financial Officer and Co-Founder<br />
 		Maryland University<br />
 		<a href="mailto:mike@bevomedia.com">mike@bevomedia.com</a>
+		<img class="unicrest" src="/Themes/BevoMedia/Images/content_execs_crest_umd.gif" alt="University of Maryland" />
 	</div>
 </blockquote>
 <div class="anno">
