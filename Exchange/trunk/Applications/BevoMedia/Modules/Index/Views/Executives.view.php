d5fe4a5ecc2773dd796e99174114f166aa8fe9a5
bobbsoap <bobbsoap@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-07-17 02:26:18 +0000

    x: exec page

--- Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Executives.view.php
+++ Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Executives.view.php
@@ -1,5 +1,5 @@
 <blockquote class="floatleft">
-	<div style="width:150px; height:200px; margin:0 15px 0 0; float:left; border:1px solid #666; background:#ccc;">pic</div>
+	<img class="floatleft" src="/Themes/BevoMedia/Images/content_execs_ryan_bukevicz.jpg" alt="Michael F. Vellucci" />
 	<div class="floatleft">
 		<h3>Ryan Bukevicz</h3>
 		President and Co-Founder<br />
@@ -9,14 +9,16 @@
 	</div>
 </blockquote>
 <div class="anno">
-	<p>As the founder and president of Bevo Media, Ryan Bukevicz has been in the online marketing industry since 2001. Bukevicz has extensive experience in various types of media buying, search and social marketing emassing revenues exceeding $10MM.  Bukevicz is also known to be involved in several other startups in internet marketing and mobile application space. During his college years, Bukevicz was a 2 sport Division 1 athlete at Syracuse University playing football and running track for the Orange. At Syracuse, Bukevicz was selected the Entrepeneur of the Year voted by university faculty. An entrepeneur at heart, Bukevicz started his first company when he was just 14 years old. Hometown located in Ivyland, PA and now resides in San Diego, California. </p>
+	<p>As the founder and president of Bevo Media, Ryan Bukevicz has been in the online marketing industry since 2001. Bukevicz has extensive experience in various types of media buying, search and social marketing emassing revenues exceeding $10MM.  Bukevicz is also known to be involved in several other startups in internet marketing and mobile application space.</p>
+	
+	<p>During his college years, Bukevicz was a 2 sport Division 1 athlete at Syracuse University playing football and running track for the Orange. At Syracuse, Bukevicz was selected the Entrepeneur of the Year voted by university faculty. An entrepeneur at heart, Bukevicz started his first company when he was just 14 years old. Hometown located in Ivyland, PA and now resides in San Diego, California. </p>
 </div>
 
 <div class="clear"></div>
 <div class="sepa"></div>
 
 <blockquote class="floatleft">
-	<div style="width:150px; height:200px; margin:0 15px 0 0; float:left; border:1px solid #666; background:#ccc;">pic</div>
+	<img class="floatleft" src="/Themes/BevoMedia/Images/content_execs_michael_chambrello.jpg" alt="Michael F. Vellucci" />
 	<div class="floatleft">
 		<h3>Michael N. Chambrello</h3>
 		Managing Partner<br />
@@ -26,14 +28,16 @@
 	</div>
 </blockquote>
 <div class="anno">
-	<p>Michael Chambrello entered the online marketing space in 2006 when he began building and selling incentivized websites, leading him to gain experience in search and social marketing. With this experience, Chambrello went on to co-found a successful local search marketing company and to become a partner in the Bevo Media Exchange. While at Syracuse University, Chambrello studied English and Textual Studies and played football for the Orange. Chambrello was recognized for academic achievement by the Big East in 2007. Chambrello is currently involved in other online business ventures, particularly in the mobile application space. Michael Chambrello is originally from Southington, CT and currently resides in San Diego, CA.</p>
+	<p>Michael Chambrello entered the online marketing space in 2006 when he began building and selling incentivized websites, leading him to gain experience in search and social marketing. With this experience, Chambrello went on to co-found a successful local search marketing company and to become a partner in the Bevo Media Exchange.</p>
+	
+	<p>While at Syracuse University, Chambrello studied English and Textual Studies and played football for the Orange. Chambrello was recognized for academic achievement by the Big East in 2007. Chambrello is currently involved in other online business ventures, particularly in the mobile application space. Michael Chambrello is originally from Southington, CT and currently resides in San Diego, CA.</p>
 </div>
 
 <div class="clear"></div>
 <div class="sepa"></div>
 
 <blockquote class="floatleft">
-	<img class="floatleft" src="/Themes/BevoMedia/Images/content_execs_michael_vellucci.gif" alt="Michael F. Vellucci" />
+	<img class="floatleft" src="/Themes/BevoMedia/Images/content_execs_michael_vellucci.jpg" alt="Michael F. Vellucci" />
 	<div class="floatleft">	
 		<h3>Michael F. Vellucci</h3>
 		Chief Financial Officer and Co-Founder<br />
@@ -43,7 +47,9 @@
 	</div>
 </blockquote>
 <div class="anno">
-	<p>Michael Vellucci has been in the online marketing industry since 2001, including co-founding Bevo Media with Ryan Bukevicz. The two have been business partners in various online ventures for over a decade. Along with his internet marketing background, Mr. Vellucci has an extensive background in finance, having worked in both investment banking and private equity. Mr. Vellucci graduated Summa Cum Laude from the University of Maryland with a double major in finance and accounting, with minor concentrations in economics and entrepreneurship.  At Maryland, Mr. Vellucci participated in the Hinman CEOs living-learning entrepreneurship program in which he won various awards and business plan competitions. Mr. Vellucci is originally from Richboro, PA, where he and Mr. Bukevicz built their first company.</p>
+	<p>Michael Vellucci has been in the online marketing industry since 2001, including co-founding Bevo Media with Ryan Bukevicz. The two have been business partners in various online ventures for over a decade. Along with his internet marketing background, Mr. Vellucci has an extensive background in finance, having worked in both investment banking and private equity.</p>
+	
+	<p>Mr. Vellucci graduated Summa Cum Laude from the University of Maryland with a double major in finance and accounting, with minor concentrations in economics and entrepreneurship.  At Maryland, Mr. Vellucci participated in the Hinman CEOs living-learning entrepreneurship program in which he won various awards and business plan competitions. Mr. Vellucci is originally from Richboro, PA, where he and Mr. Bukevicz built their first company.</p>
 </div>
 
 <div class="clear"></div>
