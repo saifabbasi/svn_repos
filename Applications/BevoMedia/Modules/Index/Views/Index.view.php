0a1cce5fde8858b6bb1ad8baaee73148258bcc9a
bobsoap <bobsoap@rcs.co>
2013-05-07 21:53:45 +0000

    exchange.bevo copy edits

--- Applications/BevoMedia/Modules/Index/Views/Index.view.php
+++ Applications/BevoMedia/Modules/Index/Views/Index.view.php
@@ -2,7 +2,7 @@
 	<?php /*<div class="bgtrans"></div>*/ ?>
 	<div class="cont">
 		<h2>Discover Bevo</h2>
-		<p>The Bevo Media Exchange features 2 platforms, The Bevo Ad Exchange and The Bevo Affiliate Portal.</p>
+		<p>The Bevo Media Exchange features two innovative marketing platforms: the Bevo Ad Exchange and the Bevo Affiliate Portal.</p>
 	</div>
 </div>
 
@@ -10,7 +10,7 @@
 	<?php /*<div class="bgtrans"></div>*/ ?>
 	<div class="cont">
 		<a class="btn logo_ads" href="http://ads.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
-		<p>The Bevo Ad Exchange is the first bidding platform to buy affiliate traffic in real time. The Bevo Ad Exchange acts as a central self served ad buying platform to buy both affiliate trafffic and display traffic from a single interface.</p>
+		<p>The Bevo Ad Exchange is the first bidding platform to buy affiliate traffic in real time. It acts as a central self-serve ad buying platform. As an advertiser, you can use the marketing expertise of some of the best-performing affiliates that run their traffic through the Bevo Affiliate Portal to your advantage, by selectively bidding on their traffic. From the same interface, you can also buy conventional display traffic from millions of high-traffic websites.</p>
 		<a class="tbtn big adsteal" href="http://ads.bevomedia.com/" target="_blank">Visit the Bevo Ad Exchange</a>
 	</div>
 </div>
@@ -18,7 +18,7 @@
 	<?php /*<div class="bgtrans"></div>*/ ?>
 	<div class="cont">
 		<a class="btn logo_aff" href="http://affportal.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
-		<p>The Bevo Media Affiliate Portal is both a free click tracking platform and an exchange that sells affiliate traffic in a real time. The tracking platform allows users to track and optimize all of their campaigns performance metrics. Users can optionally opt-in their tracking campaigns into the affiliate exchange to get paid per click for their traffic.</p>
+		<p>The Bevo Affiliate Portal is both a free click-tracking platform and an exchange that allows you to auction your affiliate traffic in real time. The integrated, feature-rich tracking platform enables you to accurately track and optimize all performance metrics of your marketing campaigns, no matter the niche or promotion method. Additionally, you can opt-in to include your campaigns into the Affiliate Exchange and get paid per click for your traffic.</p>
 		<a class="tbtn big affblue" href="http://affportal.bevomedia.com/" target="_blank">Visit the Bevo Affiliate Portal</a>
 	</div>
 </div>
@@ -28,7 +28,7 @@
 	<?php /*<div class="bgtrans"></div>*/ ?>
 	<div class="cont">
 		<h2>About Us</h2>
-		<p>The Bevo Media headquarters is located in San Diego, California. Founded in 2008, Bevo Media has grown into a multi-platform company that has created a more transparent marketplace to buy and sell performance based traffic.</p>
+		<p>The Bevo Media headquarters are located in San Diego, California. Founded in 2008, Bevo Media has grown into a multi-platform company that has created a more transparent marketplace to buy and sell performance based traffic.</p>
 	</div>
 </div>
 
