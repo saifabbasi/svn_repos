4dd187ef1a50163fcc39d2ba01120ea52aa0c415
bobsoap <bobsoap@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2013-07-04 11:44:35 +0000

    Exchange bgpic on home

--- Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Index.view.php
+++ Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Index.view.php
@@ -1,50 +1,41 @@
-<div class="box lgray">
-	<?php /*<div class="bgtrans"></div>*/ ?>
-	<div class="cont">
-		<h2>Discover Bevo</h2>
+<div class="maintop">
+	<div class="title">
+		<h2>discover bevo</h2>
 		<p>The Bevo Media Exchange features two innovative marketing platforms: the Bevo Ad Exchange and the Bevo Affiliate Portal.</p>
 	</div>
-</div>
-
-<div class="box half borderright">
-	<?php /*<div class="bgtrans"></div>*/ ?>
-	<div class="cont">
-		<a class="btn logo_ads" href="http://ads.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
-		<p>The Bevo Ad Exchange is the first bidding platform to buy affiliate traffic in real time. It acts as a central self-serve ad buying platform. As an advertiser, you can use the marketing expertise of some of the best-performing affiliates that run their traffic through the Bevo Affiliate Portal to your advantage, by selectively bidding on their traffic. From the same interface, you can also buy conventional display traffic from millions of high-traffic websites.</p>
-		<a class="tbtn big adsteal" href="http://ads.bevomedia.com/" target="_blank">Visit the Bevo Ad Exchange</a>
+	
+	<div class="box half">
+		<div class="cont">
+			<a class="btn logo_ads" href="http://ads.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
+			<p>The Bevo Ad Exchange is the first bidding platform to buy affiliate traffic in real time. It acts as a central self-serve ad buying platform. As an advertiser, you can use the marketing expertise of some of the best-performing affiliates that run their traffic through the Bevo Affiliate Portal to your advantage, by selectively bidding on their traffic. From the same interface, you can also buy conventional display traffic from millions of high-traffic websites.</p>
+			<a class="tbtn big adsteal" href="http://ads.bevomedia.com/" target="_blank">Visit the Bevo Ad Exchange</a>
+		</div>
 	</div>
-</div>
-<div class="box half last">
-	<?php /*<div class="bgtrans"></div>*/ ?>
-	<div class="cont">
-		<a class="btn logo_aff" href="http://affportal.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
-		<p>The Bevo Affiliate Portal is both a free click-tracking platform and an exchange that allows you to auction your affiliate traffic in real time. The integrated, feature-rich tracking platform enables you to accurately track and optimize all performance metrics of your marketing campaigns, no matter the niche or promotion method. Additionally, you can opt-in to include your campaigns into the Affiliate Exchange and get paid per click for your traffic.</p>
-		<a class="tbtn big affblue" href="http://affportal.bevomedia.com/" target="_blank">Visit the Bevo Affiliate Portal</a>
+	<div class="box half last">
+		<div class="cont">
+			<a class="btn logo_aff" href="http://affportal.bevomedia.com/" target="_blank">Bevo Media Ad Exchange</a>
+			<p>The Bevo Affiliate Portal is both a free click-tracking platform and an exchange that allows you to auction your affiliate traffic in real time. The integrated, feature-rich tracking platform enables you to accurately track and optimize all performance metrics of your marketing campaigns, no matter the niche or promotion method. Additionally, you can opt-in to include your campaigns into the Affiliate Exchange and get paid per click for your traffic.</p>
+			<a class="tbtn big affblue" href="http://affportal.bevomedia.com/" target="_blank">Visit the Bevo Affiliate Portal</a>
+		</div>
 	</div>
-</div>
-<div class="clear"></div>
+	<div class="clear"></div>
+</div><!--close .maintop-->
 
-<div class="box lgray">
-	<?php /*<div class="bgtrans"></div>*/ ?>
+<div class="box yell flushtop">
 	<div class="cont">
-		<h2>About Us</h2>
+		<h2>about us</h2>
 		<p>The Bevo Media headquarters are located in San Diego, California. Founded in 2008, Bevo Media has grown into a multi-platform company that has created a more transparent marketplace to buy and sell performance based traffic.</p>
 	</div>
 </div>
 
-<?php /*for bgpic later
 <script>
 $(document).ready(function() {
-	soap_imgLoader($('#bgi'), '/Themes/BevoMedia/Images/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.jpg');
+	soap_imgLoader($('#bgi'), 'http://soapdesigned.com/project/bevo/v3_img_x_nonlogin_130704/public/images/exchange/non-login/bgi.jpg');
 });
 function soap_imgLoader(div, pic) {
 	var img = new Image();
 	$(img).load(function() {
-		$(this).hide();
-		//$(div).prepend(this);
-		div.append(this);
-		$(this).fadeIn();
+		$(this).hide().prependTo(div).fadeIn(1400);
 	}).attr('src', pic);
 }
 </script>
-*/ ?>
