910c141e60f089f0b898747985c67448b26bef65
imitrikeski <imitrikeski@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2014-09-02 15:37:08 +0000

    Bug 6158

--- Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Partners.view.php
+++ Exchange/trunk/Applications/BevoMedia/Modules/Index/Views/Partners.view.php
@@ -3,6 +3,7 @@
 	<?php 
 		foreach ($this->Networks as $Network)
 		{
+			if ($Network->id==1109) continue;
 	?>
 		<img src="http://beta.bevomedia.com/Themes/BevoMedia/img/networklogos/uni/<?=$Network->id?>.png" width="150" />
 	<?php 
